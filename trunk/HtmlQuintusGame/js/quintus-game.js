f477684b055b2850365d9bc4ed8c62a8be4d107d
darek.uchman@gmail.com <darek.uchman@gmail.com@126bb808-1e85-78c8-e82a-dcf00528a34c>
2015-01-18 19:31:24 +0000

    quintus game foundadtion

--- trunk/HtmlQuintusGame/js/quintus-game.js
+++ trunk/HtmlQuintusGame/js/quintus-game.js
@@ -4,11 +4,32 @@
 
 var Q=Quintus()
     .include("Sprites")
-    .setup({width:800,height:480});
+    .setup({width:800,height:600});
 
-Q.load([],function(){
-   Q.gameLoop(function(dt){
+Q.Sprite.extend("Player",{
+       init: function(p){
 
+           this._super(p, {
+               asset: "airplane51.png",
+           x:Q.el.width / 2,
+           y:Q.el.height-60,
+           type:Q.SPRITE_FRIENDLY,
+           speed:10
+       });
+    }
+
+});
+
+
+Q.load(["spacebackground.jpg","airplane51.png"],function(){
+   var background=new Q.Sprite({asset:"spacebackground.jpg",x:Q.el.width/2,y:Q.el.height/2,type:Q.SPRITE_NONE});
+    var player= new Q.Player();
+
+
+    Q.gameLoop(function(dt){
+        Q.clear();
+       background.render(Q.ctx);
+        player.render(Q.ctx);
    }) ;
 
 });
