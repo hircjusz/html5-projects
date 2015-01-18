6ebf7ba693447818550ec77d4540c62780fb31e9
darek.uchman@gmail.com <darek.uchman@gmail.com@126bb808-1e85-78c8-e82a-dcf00528a34c>
2015-01-18 22:36:35 +0000

    quiuts

--- trunk/HtmlQuintusGame/js/quintus-game.js
+++ trunk/HtmlQuintusGame/js/quintus-game.js
@@ -6,21 +6,21 @@ var clamp = function (x, min, max) {
 }
 
 var Q = Quintus()
-    .include(["Sprites", "Anim", "Input", "Touch","Scenes"])
+    .include(["Sprites", "Anim", "Input", "Touch", "Scenes"])
     .setup({width: 800, height: 600})
     .touch();
 
-Q.input.touchEnabled=false;
-Q.input.hasTouch=true;
+Q.input.touchEnabled = false;
+Q.input.hasTouch = true;
 Q.input.touchControls({
-    controls:[
-        ['left','<'],
-        ['right','>'],
+    controls: [
+        ['left', '<'],
+        ['right', '>'],
         [],
         [],
         [],
         [],
-        ['fire','a']
+        ['fire', 'a']
     ]
 });
 
@@ -50,75 +50,90 @@ Q.Sprite.extend("Player", {
         if (Q.inputs["right"]) {
             this.p.x += this.p.speed;
         }
-        this.p.x = clamp(this.p.x, 0 + (this.p.w / 2),Q.el.width-(this.p.w/2));
-        this.fire();
+        this.p.x = clamp(this.p.x, 0 + (this.p.w / 2), Q.el.width - (this.p.w / 2));
     }
 
 });
 
 Q.Sprite.extend(
-    "Shot",{
-        init: function(p){
-            this._super(p,{
+    "Shot", {
+        init: function (p) {
+            this._super(p, {
                 //asset:'shot.png',
-                sprite:'shot',
-                sheet:'shot',
-                speed:200
+                sprite: 'shot',
+                sheet: 'shot',
+                speed: 200
 
             });
             //this.add('animation');
-           // this.play('default');
+            // this.play('default');
+        },
+        step: function (dt) {
+            this.p.y -= this.p.speed * dt;
+           if(this.p.y>Q.el.height || this.p.y<0){
+               this.destroy();
+           }
         }
+
     }
 );
-Q.component("Gun",{
-    added:function(){
-        this.entity.p.shots=[];
-
-
-
+Q.component("Gun", {
+    added: function () {
+        this.entity.p.shots = [];
+        this.entity.on('step', 'handleFiring');
     },
-    extend:{
-        fire:function(){
-         if(Q.inputs['fire']){
-             var entity=this;
-             var shot=Q.stage().insert(new Q.Shot({x:this.p.x+60,y:this.p.y-50,speed:200,type:Q.SPRITE_DEFAULT|Q.SPRITE_FRIENDLY}));
-             this.p.shots.push(shot);
-         }
+    extend: {
+        handleFiring: function (dt) {
 
+            var entity =this;
+            for(var i=entity.p.shots.length-1;i>=0;i--){
+                if(entity.p.shots[i].isDestroyed){
+                    entity.p.sheet.splice(i,1);
+                }
+            }
+
+            if (Q.inputs['fire']) {
+                this.fire();
+            }
+        },
+        fire: function () {
+            var entity = this;
+            var shot = Q.stage().insert(new Q.Shot({
+                x: this.p.x + 60,
+                y: this.p.y - 50,
+                speed: 200,
+                type: Q.SPRITE_DEFAULT | Q.SPRITE_FRIENDLY
+            }));
+            this.p.shots.push(shot);
         }
     }
-
-
-
-
 });
 
-Q.scene("mainLevel",function(stage){
-    Q.gravity=0;
-    stage.insert(new  Q.Sprite({
-        asset: "spacebackground.jpg",
-        x: Q.el.width / 2,
-        y: Q.el.height / 2,
-        type: Q.SPRITE_NONE
-    })
+Q.scene("mainLevel", function (stage) {
+    Q.gravity = 0;
+    stage.insert(new Q.Sprite({
+            asset: "spacebackground.jpg",
+            x: Q.el.width / 2,
+            y: Q.el.height / 2,
+            type: Q.SPRITE_NONE
+        })
     );
     stage.insert(new Q.Player());
-    stage.insert(new Q.Shot({x:100,y:100}));
+    stage.insert(new Q.Shot({x: 100, y: 100}));
 });
 
-Q.load(["spacebackground.jpg", "airplane51.png", "shot.png","player.json","shot.json"], function () {
+Q.load(["spacebackground.jpg", "airplane51.png", "shot.png", "player.json", "shot.json"], function () {
     Q.compileSheets("shot.png", "shot.json");
     Q.compileSheets("airplane51.png", "player.json");
     Q.animations("player", {default: {frames: [0, 1, 2, 3, 4], rate: 1 / 4}});
     Q.animations("shot", {default: {frames: [0, 1, 2, 3, 4], rate: 1 / 4}});
     Q.stageScene("mainLevel");
 
-   /* Q.gameLoop(function (dt) {
-        Q.clear();
-        background.render(Q.ctx);
-        player.update(dt);
-        player.render(Q.ctx);
-    });*/
+    /* Q.gameLoop(function (dt) {
+     Q.clear();
+     background.render(Q.ctx);
+     player.update(dt);
+     player.render(Q.ctx);
+     });*/
 
 });
