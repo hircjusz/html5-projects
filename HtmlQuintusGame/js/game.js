a7833adb85ef9c0ee15e10a309affa609187658e
darek.uchman@gmail.com <darek.uchman@gmail.com@126bb808-1e85-78c8-e82a-dcf00528a34c>
2015-01-18 17:52:19 +0000

    game

--- HtmlQuintusGame/js/game.js
+++ HtmlQuintusGame/js/game.js
@@ -7,6 +7,26 @@ var canvasHeight=600;
 $('#gameCanvas').attr('width',canvasWidth);
 $('#gameCanvas').attr('width',canvasHeight);
 
+
+var keysDown={};
+
+$('body').bind('keydown',function(e){
+
+keysDown[e.which]=true;
+
+
+});
+
+$('body').bind('keyup',function(e){
+
+    keysDown[e.which]=false;
+
+
+});
+
+
+
+
 var canvas=$('#gameCanvas')[0].getContext('2d');
 
 var image= new Image();
@@ -25,9 +45,32 @@ function(){
 },1000/FPS
 );
 
+
+
     function update(){
-    playerX++;
+    if(keysDown[37]){
+        playerX-=10;
+    }
+    if(keysDown[38]){
+
+        playerY-=10;
+    }
+        if(keysDown[39]){
+            playerX+=10;
+        }
+        if(keysDown[40]){
+
+            playerY+=10;
+        }
+       playerX= clamp(playerX,0,canvasWidth-image.width);
+        playerY=clamp(playerY,0,canvasHeight-image.height);
 }
+
+
+function clamp(x,min,max){
+    return x<min?min:(x>max?max:x);
+}
+
 
 function draw(){
     canvas.clearRect(0,0,canvasWidth,canvasHeight);
