31aba41263664563d657d597c6e080ae10e8eb95
darek.uchman@gmail.com <darek.uchman@gmail.com@126bb808-1e85-78c8-e82a-dcf00528a34c>
2015-01-18 18:22:01 +0000

    quitus library

---
+++ trunk/HtmlQuintusGame/js/quintus-game.js
@@ -0,0 +1,14 @@
+/**
+ * Created by Darek on 2015-01-18.
+ */
+
+var Q=Quintus()
+    .include("Sprites")
+    .setup({width:800,height:480});
+
+Q.load([],function(){
+   Q.gameLoop(function(dt){
+
+   }) ;
+
+});
