c8b3cf718fdffbdd4d9a1e4aaa2d8865ac887a2f
darek.uchman@gmail.com <darek.uchman@gmail.com@126bb808-1e85-78c8-e82a-dcf00528a34c>
2015-01-19 22:11:21 +0000

    plugin development

--- extJSProject/js/ch02-extjs.js
+++ extJSProject/js/ch02-extjs.js
@@ -1,3 +1,24 @@
 /**
  * Created by Darek on 2015-01-19.
  */
+Ext.onReady(function(){
+
+    Ext.define('Examples.ux.LabeledSpinner', {
+        extend : 'Ext.form.field.Spinner',
+        alias : 'widget.labeledspinner',
+        onSpinUp : function() {
+            this.setValue(++this.value);
+        },
+        onSpinDown : function() {
+            this.setValue(--this.value);
+        }
+    });
+    Ext.create('widget.labeledspinner',{
+        renderTo:Ext.getBody()
+
+    });
+
+
+
+
+});
