d57d4a50d86b4e61d95b2f68059c8a866c3a6f11
DESKTOP-HPRUNBD\Darek <darek.uchman@gmail.com>
2017-09-24 15:11:31 +0200

    testimonials

--- Html5/Udemy/Vesco/js/custom.js
+++ Html5/Udemy/Vesco/js/custom.js
@@ -28,3 +28,16 @@ $(function () {
 $(function () {
     $("#team-members").owlCarousel();
 });
+
+/* =========================================================
+           TESTIMONIALS
+============================================================*/
+$(function () {
+    $("#customers-testimonials").owlCarousel({
+        items: 1,
+        autoplay: true,
+        smartSpeed: 700,
+        loop: true,
+        autoplayHoverPause: true
+    });
+});
