8de96670ea70e26df6b0bade820e73e60628a081
DESKTOP-HPRUNBD\Darek <darek.uchman@gmail.com>
2017-09-24 23:17:57 +0200

    Clients section

--- Html5/Udemy/Vesco/js/custom.js
+++ Html5/Udemy/Vesco/js/custom.js
@@ -41,3 +41,16 @@ $(function () {
         autoplayHoverPause: true
     });
 });
+
+/* =========================================================
+         CLIENTS
+============================================================*/
+$(function () {
+    $("#clients-list").owlCarousel({
+        items: 6,
+        autoplay: true,
+        smartSpeed: 700,
+        loop: true,
+        autoplayHoverPause: true
+    });
+});
